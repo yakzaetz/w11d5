a5ebf764c36ac008b2551b90b4db42be15573e93
yakzaetz <yakzaetz@gmail.com>
2022-02-18 12:04:28 -0500

    routes completed

--- skeleton/config/routes.rb
+++ skeleton/config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # Your routes here!
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    resources :gifts, only: [:index, :show]
+    resources :guests, only: [:index, :show]
+    resources :parties, only: [:index, :show]
+  end
+ 
 end
